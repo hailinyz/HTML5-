03f41a494b977af3944ba17b0dc4e23406b1414e
hailinyz <1660246929@qq.com>
2025-11-19 12:46:30 +0800

    更新视频为网络资源链接

--- src/stores/musicStore.js
+++ src/stores/musicStore.js
@@ -9,39 +9,39 @@ export const currentSongIndex = ref(0)
 export const songs = ref([
   {
     title: "晴天",
-    src: "https://tc.hailin.ip-ddns.com/file/OSS/1760617688256_晴天_-_周杰伦.mp3"
+    src: "https://javawebhl.oss-cn-beijing.aliyuncs.com/2025/%E6%99%B4%E5%A4%A9%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.mp3"
   },
   {
     title: "七里香",
-    src: "https://tc.hailin.ip-ddns.com/file/OSS/1760621490165_七里香_-_周杰伦.mp3"
+    src: "https://javawebhl.oss-cn-beijing.aliyuncs.com/2025/%E4%B8%83%E9%87%8C%E9%A6%99%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.mp3"
   },
   {
     title: "青花瓷",
-    src: "https://tc.hailin.ip-ddns.com/file/OSS/1760621488982_青花瓷_-_周杰伦.mp3"
+    src: "https://javawebhl.oss-cn-beijing.aliyuncs.com/2025/%E9%9D%92%E8%8A%B1%E7%93%B7%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.mp3"
   },
   {
     title: "花海",
-    src: "https://tc.hailin.ip-ddns.com/file/OSS/1760621494002_花海_-_周杰伦.mp3"
+    src: "https://javawebhl.oss-cn-beijing.aliyuncs.com/2025/%E8%8A%B1%E6%B5%B7%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.mp3"
   },
   {
     title: "反方向的钟",
-    src: "https://tc.hailin.ip-ddns.com/file/OSS/1760621494322_反方向的钟_-_周杰伦.mp3"
+    src: "https://javawebhl.oss-cn-beijing.aliyuncs.com/2025/%E5%8F%8D%E6%96%B9%E5%90%91%E7%9A%84%E9%92%9F%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.mp3"
   },
   {
     title: "珊瑚海",
-    src: "https://tc.hailin.ip-ddns.com/file/OSS/1760621508629_珊瑚海_-_周杰伦_梁心颐.mp3"
+    src: "https://javawebhl.oss-cn-beijing.aliyuncs.com/2025/%E7%8F%8A%E7%91%9A%E6%B5%B7%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6_%E6%A2%81%E5%BF%83%E9%A2%90.mp3"
   },
   {
     title: "说了再见",
-    src: "https://tc.hailin.ip-ddns.com/file/OSS/1760621508332_说了再见_-_周杰伦.mp3"
+    src: "https://javawebhl.oss-cn-beijing.aliyuncs.com/2025/%E8%AF%B4%E4%BA%86%E5%86%8D%E8%A7%81%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.mp3"
   },
   {
     title: "爱在西元前",
-    src: "https://tc.hailin.ip-ddns.com/file/OSS/1760621491387_爱在西元前_-_周杰伦.mp3"
+    src: "https://javawebhl.oss-cn-beijing.aliyuncs.com/2025/%E7%88%B1%E5%9C%A8%E8%A5%BF%E5%85%83%E5%89%8D%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.mp3"
   },
   {
     title: "红尘客栈",
-    src: "https://tc.hailin.ip-ddns.com/file/OSS/1760621494516_红尘客栈_-_周杰伦.mp3"
+    src: "https://javawebhl.oss-cn-beijing.aliyuncs.com/2025/%E7%BA%A2%E5%B0%98%E5%AE%A2%E6%A0%88%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.mp3"
   },
 ])
 
